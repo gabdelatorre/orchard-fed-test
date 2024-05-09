cd6c27b13e223ceb3c9f66d784d95d11e58771d1
gabdelatorre <gab.delatorre@xendit.co>
2024-05-09 17:07:27 +0800

    Initial commit for nextjs project setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,12 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      screens: {
+        xs: "320px",
+      },
+      colors: {
+        "orchard-black": "#0e1414",
+        "orchard-red": "#cf1430",
       },
     },
   },
